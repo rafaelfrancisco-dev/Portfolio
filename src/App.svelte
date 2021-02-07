de972b4d8106034ce2f25b9754ab894461aad16a
Rafael Francisco <rafaelfrancisco6@gmail.com>
2021-02-07 18:33:43 +0000

    #1 commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import MainComponent from "./Components/MainComponent.svelte";
+	import DarkModeToggle from "./Components/DarkModeToggle.svelte";
+</script>
+
+<main class="bg-white dark:bg-black text-white dark:text-black">
+	<DarkModeToggle/>
+	<MainComponent/>
+</main>
+
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
---
+++ src/Components/DarkModeToggle.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import {onMount} from "svelte";
+
+    onMount(async () => {
+        document.documentElement.classList.add('light');
+    });
+
+    function toggle() {
+        if (document.documentElement.classList.contains('light')) {
+            document.documentElement.classList.remove('light');
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            document.documentElement.classList.add('light');
+        }
+
+    }
+</script>
+
+<div>
+    <button class="bg-gray-200 dark:bg-gray-800 rounded p-3 h-10 w-10" on:click={toggle}>
+        <slot/>
+    </button>
+</div>
