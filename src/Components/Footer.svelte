640b9e3ea46806bcd6b297cb558b72c587c77304
Rafael Francisco <rafaelfrancisco6@gmail.com>
2021-02-10 19:38:06 +0000

    More changes

--- src/Components/Footer.svelte
+++ src/Components/Footer.svelte
@@ -1,5 +1,28 @@
-<footer class="flex flex-col justify-center items-start max-w-2xl mx-auto w-full mb-8">
+<script lang="ts">
+    let elements = [
+        {
+            name: "Github",
+            link: "https://github.com/rafaelfrancisco-dev"
+        },
+        {
+            name: "LinkedIn",
+            link: "https://www.linkedin.com/in/rafael-francisco/"
+        },
+        {
+            name: "Twitter",
+            link: "https://twitter.com/does_it_compile"
+        },
+        {
+            name: "Instagram",
+            link: "https://www.instagram.com/rafaelfrancisco6"
+        }
+    ]
+</script>
+
+<footer class="flex flex-col justify-center items-start max-w-2xl mx-auto w-full mb-4">
     <div class="w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 sm:grid-cols-3">
-        <a class="text-gray-500 hover:text-gray-600 transition">Github</a>
+        {#each elements as footerItem}
+            <a class="text-gray-500 hover:text-gray-600 transition" href={footerItem.link} target="_blank">{footerItem.name}</a>
+        {/each}
     </div>
 </footer>
--- src/Components/TimelineComponent.svelte
+++ src/Components/TimelineComponent.svelte
@@ -1,29 +1,34 @@
 <script lang="ts">
     let timelineElements = [
         {
-            title: "asd",
-            description: "asd",
-            year: 2019
+            title: "Released <i>Today in History</i> for iOS",
+            description: "Launched the first one of my rewritten existing apps for iOS. Published by Andi.",
+            year: 2018
         },
         {
-            title: "asd",
-            description: "asd",
-            year: 2019
-        },
-        {
-            title: "fghfghfgh",
-            description: "fghfghfghf",
-            year: 2019
-        },
-        {
-            title: "fgh",
-            description: "fghfghfghfgh",
-            year: 2019
-        },
-        {
-            title: "fgh",
-            description: "fghfghgfhfgh",
+            title: "Released <i>Today in History</i> for Android",
+            description: "A really simple side-project, which combines my love of small, uncomplicated apps and history.",
             year: 2017
+        },
+        {
+            title: "Started working full-time as an iOS Dev",
+            description: "Got hired as full-time iOS dev following the end of the internship.",
+            year: 2017
+        },
+        {
+            title: "Released <i>Fogos em Portugal</i> for Android",
+            description: "Released the first version of Fogos em Portugal, my first hobby app, to compete with the, then at the time, <i>lacking</i> competitors.",
+            year: 2017
+        },
+        {
+            title: "Began career",
+            description: "Started as WatchKit intern at YOUSHIP, creating an Apple Watch extension for the existing app.",
+            year: 2016
+        },
+        {
+            title: "Initiated studies",
+            description: "Enrolled in Computer Sciences at <b>Instituto Superior de Engenharia de Coimbra</b>.",
+            year: 2013
         }
     ]
 </script>
@@ -42,10 +47,10 @@
                             <g fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path><path d="M22 4L12 14.01l-3-3"></path></g>
                         </svg>
 
-                        <p class="font-medium text-gray-900 dark:text-gray-100">{title}</p>
+                        <p class="font-medium text-gray-900 dark:text-gray-100">{@html title}</p>
                     </div>
 
-                    <p class="text-gray-700 dark:text-gray-400 ml-6">{description}</p>
+                    <p class="text-gray-700 dark:text-gray-400 ml-6">{@html description}</p>
                 </li>
             {/each}
 
